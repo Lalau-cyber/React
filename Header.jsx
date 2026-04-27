2fd95e46c926d917b095515e59705d552f50311d
Laura <laurinha.ss1305@gmail.com>
2026-04-27 15:42:05 -0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+
+function Header(){
+    return(
+        <header style={{backgroundColor: 'lightgray', padding: '20px', textAlign: 'center'}}>
+            <h1>Bem-Vindo</h1>
+            <p>Este é o meu site onde vc acha filmes</p>
+        </header>
+
+    )
+
+}
+
+export default Header;
